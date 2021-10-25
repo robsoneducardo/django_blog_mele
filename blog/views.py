a5f5ba1f1fd39b4aa9fb2f81a2354f412432b7eb
robsoneducardo <robsoneducardo@gmail.com>
2021-10-25 13:04:37 -0300

    created post_list class based

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
 
 def post_list(request):
     posts = Post.published.all()
@@ -13,3 +14,9 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
